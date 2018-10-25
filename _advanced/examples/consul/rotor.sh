3af5d47a41d0463768b9ad3717702107519849f8
Glen D Sanford <9len@users.noreply.github.com>
2018-10-25 16:21:54 +0000

    prepare for release of OSS version 0.19.0 (#6061)
    fbshipit-source-id: 1f1cf02

--- _advanced/examples/consul/rotor.sh
+++ _advanced/examples/consul/rotor.sh
@@ -4,4 +4,4 @@ docker run -d \
   -e "ROTOR_CMD=consul" \
   -e "ROTOR_CONSUL_DC=dc1" \
   -e "ROTOR_CONSUL_HOSTPORT=<consul ip address>:8500" \
-  turbinelabs/rotor:0.18.2
+  turbinelabs/rotor:0.19.0
